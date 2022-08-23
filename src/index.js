d677fa6e8efddc1ef094070b6cc82f88425cadbe
Rodolfo P. Grião <103333634+RodolfoPG@users.noreply.github.com>
2022-08-23 17:16:36 -0300

    Estrutura do projeto

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-ReactDOM.render(<h1>App</h1>, document.getElementById('root'))
+import App from './views/App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
