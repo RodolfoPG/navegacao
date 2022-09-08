2342ecc9375b4e762c4f0d5ddda234f8430b3def
Rodolfo P. Grião <103333634+RodolfoPG@users.noreply.github.com>
2022-09-08 17:31:18 -0300

    Pagina nao encontrada

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "../../views/examples/Home";
 import Param from '../../views/examples/Param';
 import About from "../../views/examples/About";
+import NotFound from '../../views/examples/NotFound';
  
 const Content = props => (
   <main className="Content">
@@ -12,6 +13,7 @@ const Content = props => (
       <Route path="/about" element={<About />} />
       <Route path="/param/:id" exact element={<Param />} />
       <Route path="/" exact element={<Home />} />
+      <Route path="*" exact element={<NotFound />} />
     </Routes>
   </main>
 )
