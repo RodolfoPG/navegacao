c77825b268ca3419bdaa10a2cd5a873e19495b31
Rodolfo P. Grião <103333634+RodolfoPG@users.noreply.github.com>
2022-09-08 12:15:37 -0300

    Navegaçao simples

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -3,17 +3,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom"
 
 import Home from "../../views/examples/Home";
- import Param from "../../views/examples/Param";
- import About from "../../views/examples/About";
- import NotFound from "../../views/examples/NotFound";
+import About from "../../views/examples/About";
+ 
 
 const Content = props => (
   <main className="Content">
     <Routes>
       <Route path="/about" element={<About />} />
-      <Route path="/param/:id" element={<Param />} />
       <Route path="/" exact element={<Home />} />
-      <Route path="*" element={<NotFound />} />
     </Routes>
   </main>
 )
